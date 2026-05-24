d89396be93989c1b5b09cb9d9f98198231a640e4
Christian <chr.astorga@duocuc.cl>
2026-05-24 17:17:52 -0400

    implementacion completa de la suite de pruebas y lógica de negocio de inventario

--- Inventory-service/src/main/java/cl/duoc/gamehub/inventory/client/ProductoClient.java
+++ Inventory-service/src/main/java/cl/duoc/gamehub/inventory/client/ProductoClient.java
@@ -3,11 +3,12 @@ package cl.duoc.gamehub.inventory.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import java.util.Map;
 
-@FeignClient(name = "product-service", url = "http://localhost:8082/api/productos")
+
+@FeignClient(name = "product-service", url = "http://localhost:8082")
 public interface ProductoClient {
 
-    @GetMapping("/buscar/{id}")
-    Map<String, Object> buscarPorId(@PathVariable("id") Long id);
+
+    @GetMapping("/api/productos/buscar/{id}")
+    void  buscarPorId(@PathVariable("id") Long id);
 }
