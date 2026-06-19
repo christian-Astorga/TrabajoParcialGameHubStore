ac14bd27d71d1b66390ab440574672a8db384e04
Christian <chr.astorga@duocuc.cl>
2026-06-18 20:14:29 -0400

    implementar microservicio de review con HATEOAS, FeignClient y Eureka, leve modificacion en POM de promotion para su correcto funcionamiento

--- promotion-service/src/main/java/cl/duoc/gamehub/promotion/ProductClient.java
+++ promotion-service/src/main/java/cl/duoc/gamehub/promotion/ProductClient.java
@@ -1,13 +1,13 @@
-package cl.duoc.gamehub.promotion;
+package cl.duoc.gamehub.review.client; // Paquete de REVIEW
 
 import cl.duoc.gamehub.promotion.dto.ProductExternalDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product-service", url = "http://localhost:8081/api/productos")
+@FeignClient(name = "product-service")
 public interface ProductClient {
 
-    @GetMapping("/buscar/{id}")
+    @GetMapping("/api/productos/buscar/{id}")
     ProductExternalDTO buscarProductoPorId(@PathVariable("id") Long id);
 }
