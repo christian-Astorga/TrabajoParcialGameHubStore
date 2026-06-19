fe5f9241db63512a969a46c48a0e1657cbaacd9a
Christian <chr.astorga@duocuc.cl>
2026-06-18 20:20:57 -0400

    modificacion de POM en auth, errores de feign corregidos

--- auth-service/src/main/java/cl/duoc/gamehub/auth/AuthApplication.java
+++ auth-service/src/main/java/cl/duoc/gamehub/auth/AuthApplication.java
@@ -3,11 +3,11 @@ package cl.duoc.gamehub.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients; // <-- Este import ahora se pondrá gris/normal, ya no rojo
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+@EnableFeignClients     
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
